e558d4ddd06c5fb1318f1a784dbbc0a87ee3d785
abusuffian <126444140+abusuffian@users.noreply.github.com>
2023-03-04 19:20:34 +0600

    added

---
+++ index.php
@@ -0,0 +1,659 @@
+<?php phpinfo();  ?>
+
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+  <meta charset="utf-8">
+  <title>app business</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" media="all" />
+  <link rel="stylesheet" type="text/css" href="css/normalize.css" media="all" />
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" media="all" />
+  <link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css" media="all" />
+  <link rel="stylesheet" type="text/css" href="css/uikit-rtl.min.css" media="all" />
+  <link rel="stylesheet" type="text/css" href="css/uikit.min.css" media="all" />
+  <link rel="stylesheet" type="text/css" href="style.css" media="all" />
+  <link rel="shortcut icon" type="images/x-icon" href="images/app1.ico" media="all" />
+  
+  <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet"> 
+</head>
+<body>
+
+<header class="header-area">
+ 
+  <div class="header-left " uk-sticky="top: 100">
+  
+  <div class="container">
+  
+  <div class="logo-area">
+  
+  <a href="#"><img src="images/logo.png" alt="Logo" /></a>
+  
+  </div>
+  
+  <nav class="main-menu">
+  	<ul>
+  		<li><a class="active" href="#">HOME</a></li>
+  		<li><a href="#servecies">servecies</a></li>
+  		<li><a href="#">WORK</a></li>
+  		<li><a href="http://localhost/hemel/blog.php/admin/login.php">BLOG</a></li>
+  		<li><a href="#">CONTACT</a></li>
+  	</ul>
+  </nav>
+ 
+  </div>
+  </div>
+  <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="images/s10.jpg" alt="...">
+      <div class="carousel-caption">
+        <div class="header-writhing">
+  
+  <h2>Hi there! We are the new kids on the block </h2>
+<h2>and we build awesome websites and mobile apps.</h2>
+  
+ 
+  <div class="input-box">
+  
+   <a href="#">WORK WITH US!</a>
+   
+  </div>
+ </div>
+      </div>
+    </div>
+    <div class="item">
+      <img src="images/s1.jpg" alt="...">
+      <div class="carousel-caption">
+        <div class="header-writhing">
+  
+  <h2> We are the new kids on the block </h2>
+<h2>and we build awesome websites and mobile apps.</h2>
+  
+  
+  
+  <div class="input-box">
+  
+   <a href="#">WORK WITH US!</a>
+   
+  </div>
+ </div>
+      </div>
+    </div>
+	 <div class="item">
+      <img src="images/s2.jpg" alt="...">
+      <div class="carousel-caption">
+        <div class="header-writhing">
+  
+  <h2>Hi there! We are the new kids on the block </h2>
+<h2>and we build awesome websites and mobile apps.</h2>
+  
+  
+  
+  <div class="input-box">
+  
+   <a href="#">WORK WITH US!</a>
+   
+  </div>
+ </div>
+      </div>
+    </div>
+	 <div class="item">
+      <img src="images/s3.jpg" alt="...">
+      <div class="carousel-caption">
+        <div class="header-writhing">
+  
+  <h2>Hi there! We are the new kids on the block </h2>
+<h2>and we build awesome websites and mobile apps.</h2>
+  
+  
+  
+  <div class="input-box">
+  
+   <a href="#">WORK WITH US!</a>
+   
+  </div>
+ </div>
+      </div>
+    </div>
+	
+	 <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+	
+ 
+  </div>
+  </div>
+  
+</header>
+
+
+
+
+
+
+<!-- header-area end -->
+<!-- servecies-area start -->
+
+<section data-spy="scroll" id="servecies" class="services-area">
+<div class="container">
+<h2>SERVICES WE PROVIDE</h2>
+<hr />
+<p>We are working with both individuals and businesses from all over the globe 
+to create awesome websites and applications.
+</p>
+
+<div class="services-boxs row">
+
+<div class="box col-lg-3 col-md-3 col-sm-6" uk-scrollspy="cls: uk-animation-slide-left; repeat: true">
+
+<img src="images/a1.png" alt="logo" />
+<h2>Branding</h2>
+<p>Lorem ipsum dolor sit amet, 
+consectetuer adipiscing elit, sed diam nonummy nibh.</p>
+
+</div>
+
+<div class="box col-lg-3 col-md-3 col-sm-6" uk-scrollspy="cls: uk-animation-slide-top; repeat: true">
+
+<img src="images/a2.png" alt="logo" />
+<h2>DESIGN</h2>
+<p>Lorem ipsum dolor sit amet, 
+consectetuer adipiscing elit, sed diam nonummy nibh.</p>
+
+</div>
+
+<div class="box col-lg-3 col-md-3 col-sm-6" uk-scrollspy="cls: uk-animation-slide-bottom; repeat: true">
+
+<img src="images/a3.png" alt="logo" />
+<h2>DEVLOPMENT</h2>
+<p>Lorem ipsum dolor sit amet, 
+consectetuer adipiscing elit, sed diam nonummy nibh.</p>
+
+</div>
+
+<div class="box col-lg-3 col-md-3 col-sm-6" uk-scrollspy="cls: uk-animation-slide-right; repeat: true">
+
+<img src="images/a4.png" alt="logo" />
+<h2>ROCKIT SCIENCE</h2>
+<p>Lorem ipsum dolor sit amet, 
+consectetuer adipiscing elit, sed diam nonummy nibh.</p>
+
+</div>
+</div>
+</div>
+</section>
+
+<!-- services-area end -->
+<!-- team-area start -->
+
+<section class="team-area">
+<div class="container">
+<h2>MEET OUR BEAUTIFUL TEAM</h2>
+<hr />
+<p>We are a small team of designers and developers, who help brands with big ideas.
+</p>
+
+<div class="team-boxs row">
+
+<div class="box col-lg-3 col-md-3 col-sm-6">
+<div class="team-mem">
+<img src="images/h1.png" alt="logo" />
+</div>
+<h2>BILLGATES</h2>
+<h3>CEO / Marketing Guru</h3>
+<p>William Henry Gates III (born October 28, 1955) is an American business magnate, investor, philanthropist, and writer. He is a co-founder of Microsoft, along with his late childhood friend Paul Allen.[2][3] During his career at Microsoft, Gates held the positions of chairman, chief executive officer (CEO), president and chief software architect, while also being the largest individual shareholder until May 2014.[4] He was a major entrepreneur of the microcomputer revolution of the 1970s and 1980s. </p>
+<div class="social-link">
+<ul>
+	<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+	<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+	<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+	<li><a href="#"><i class="fa fa-envelope"></i></a></li>
+</ul>
+</div>
+</div>
+
+<div class="box col-lg-3 col-md-3 col-sm-6">
+<div class="team-mem">
+<img src="images/h2.png" alt="logo" />
+</div>
+<h2>WAREN BARAT</h2>
+<h3>Creative Director</h3>
+<p>Warren Barat is the General Manager at Electrical Reticulation Design Services. Additionally, Warren Barat has had 1 past job as the Group Manager - Sales & Customer Engagement at Transgrid.</p>
+<div class="social-link">
+<ul>
+  <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+	<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+	<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+	<li><a href="#"><i class="fa fa-envelope"></i></a></li>
+</ul>
+</div>
+</div>
+
+<div class="box col-lg-3 col-md-3 col-sm-6">
+<div class="team-mem">
+<img src="images/h3.png" alt="logo" />
+</div>
+<h2>IMAM</h2>
+<h3>Lead Designer</h3>
+<p>imam, Arabic imām (“leader,” “model”), in a general sense, one who leads Muslim worshippers in prayer. In a global sense, imam is used to refer to the head of the Muslim community (ummah).</p>
+<div class="social-link">
+<ul>
+	<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+	<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+	<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+	<li><a href="#"><i class="fa fa-envelope"></i></a></li>
+</ul>
+</div>
+</div>
+
+<div class="box col-lg-3 col-md-3 col-sm-6">
+<div class="team-mem">
+<a href="https://www.facebook.com/suffian.hem"><img src="images/c4.png" alt="logo" /> </a>
+
+</div>
+<h2>Abu Suffian Hemel</h2>
+<h3>SEO / Developer</h3>
+<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+<div class="social-link">
+<ul>
+	<li><a href="error field."><i class="fa fa-facebook"></i></a></li>
+	<li><a href="#"><i class="fa fa-twitter"></i></a></li>	
+	<li><a href="#"><i class="fa fa-envelope"></i></a></li>
+</ul>
+</div>
+</div>
+
+
+</div>
+</div>
+</section>
+
+<!-- skills-area start -->
+
+<section class="skills-area">
+<div class="container">
+<h2>WE GOT SKILLS!</h2>
+<hr />
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod 
+tempor incididunt ut labore et dolore magna aliqua.</p>
+
+<div class="skills-boxes row">
+
+<div class="box col-lg-3 col-md-3 col-sm-6">
+
+<div class="sk1 pie-title-center"  data-percent="90" >
+ <span class="pie-value">90%</span>
+</div>
+
+<h2>WEB DESIGN</h2>
+</div>
+
+<div class="box col-lg-3 col-md-3 col-sm-6">
+<div class="sk2 pie-title-center"  data-percent="75" >
+ <span class="pie-value">75%</span>
+</div>
+<h2>HTML/CSS</h2>
+</div>
+
+<div class="box col-lg-3 col-md-3 col-sm-6">
+<div class="sk3 pie-title-center"  data-percent="70" >
+ <span class="pie-value">70%</span>
+</div>
+<h2>GRAPHIC DESIGN</h2>
+</div>
+
+<div class="box col-lg-3 col-md-3 col-sm-6">
+<div class="sk4 pie-title-center"  data-percent="85" >
+ <span  class="pie-value">85%</span>
+</div>
+<h2>UL/UX</h2>
+</div>
+
+</div>
+</div>
+</section>
+
+<!-- protfloio-area start -->
+
+<section class="protfloio-area">
+<div class="container">
+<h2>OUR PORTFOLIO</h2>
+<hr />
+<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet
+ consectetur, adipisci velit, sed quia non numquam</p>
+
+ <nav class="portfolio-menu">
+ 	<ul>
+ 		<li><a class="active-with"  data-filter="*">All</a></li>
+ 		<li><a  data-filter=".web">Web</a></li>
+ 		<li><a  data-filter=".apps">Apps</a></li> 
+ 		<li><a data-filter=".icons">Icons</a></li>
+ 	</ul>
+ </nav>
+ 
+ 
+<div class="protfloio-boxes row">
+<div class="half">
+<div class="box mix web apps col-lg-6 col-md-6">
+<div class="port-image">
+<img src="images/b1.png" alt="Logo" />
+</div>
+<h2>Isometric Perspective Mock-Up</h2>
+</div>
+
+<div class="box mix apps icons col-lg-6 col-md-6">
+<div class="port-image">
+<img src="images/b2.png" alt="Logo" />
+</div>
+<h2>Time Zone App UI</h2>
+</div>
+</div>
+
+<div class="bot-half">
+
+<div class="box mix web col-lg-6 col-md-6">
+<div class="port-image">
+<img src="images/b3.png" alt="Logo" />
+</div>
+<h2>Viro Media Players UI</h2>
+</div>
+
+<div class="box mix apps icons col-lg-6 col-md-6">
+<div class="port-image">
+<img src="images/b4.png" alt="Logo" />
+</div>
+<h2>Blog / Magazine Flat UI Kit</h2>
+</div>
+</div>
+
+<div class="input-port">
+  
+   <a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiYg73L19jyAhUFWCsKHelLBtUQFnoECAMQAQ&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Flaptop%2F&usg=AOvVaw01UHjU2MmOVSIh9s3t3jMy">LOAD MORE PROJECTS</a>
+   
+  </div>
+
+</div> 
+</div>
+</section>
+
+ <!-- testimonal area start -->
+
+<section class="testimonal-area">
+<div class="container">
+<h2>WHAT POEPLE SAY ABOUT US</h2>
+<hr />
+<p>Our clients love us!</p>
+
+ 
+<div class="testimonal-boxes row">
+
+<div class="box col-lg-6 col-md-6">
+<div class="testi-image">
+<img src="images/h1.png" alt="Logo" />
+</div>
+<div class="testi-cont">
+<q>William Henry Gates III (born October 28, 1955) is an American business magnate, investor, philanthropist, and writer. He is a co-founder of Microsoft, along with his late childhood friend Paul Allen.[2][3] During his career at Microsoft, Gates held the positions of chairman, chief executive officer (CEO), president and chief software architect, while also being the largest individual shareholder until May 2014.[4] He was a major entrepreneur of the microcomputer revolution of the 1970s and 1980s. </q>
+ <h2>BILLGATES</h2>
+ <h3>CEO of Pinterest</h3>
+</div>
+</div>
+
+<div class="box col-lg-6 col-md-6">
+<div class="testi-image">
+<img src="images/h2.png" alt="Logo" />
+</div>
+<div class="testi-cont">
+<q>Warren Barat is the General Manager at Electrical Reticulation Design Services. Additionally, Warren Barat has had 1 past job as the Group Manager - Sales & Customer Engagement at Transgrid.</q>
+ <h2>WAREN BARAT
+</h2>
+ <h3>Founder of Instagram
+</h3>
+</div>
+</div>
+
+<div class="box col-lg-6 col-md-6">
+<div class="testi-image">
+<img src="images/h3.png" alt="Logo" />
+</div>
+<div class="testi-cont">
+<q>imam, Arabic imām (“leader,” “model”), in a general sense, one who leads Muslim worshippers in prayer. In a global sense, imam is used to refer to the head of the Muslim community (ummah).
+
+</q>
+ <h2>IMAM
+</h2>
+ <h3>Lead Designer at Behance
+</h3>
+</div>
+</div>
+
+<div class="box col-lg-6 col-md-6">
+<div class="testi-image">
+<img src="images/h4.png" alt="Logo" />
+</div>
+<div class="testi-cont">
+<q>Phasellus non purus vel arcu tempor commodo. Fusce semper, purus vel luctus molestie, risus sem cursus neque.
+
+</q>
+ <h2>MUSA SOMSHER
+</h2>
+ <h3>Co-Founder of Shazam
+</h3>
+</div>
+</div>
+
+</div>
+</div> 
+</section>
+
+ <!-- testimonal area end -->
+ <!-- Card area start -->
+ <section class="Card-area">
+
+<h2>How to design cards for websites and web apps?</h2>
+<hr />
+<p>Our clients love us!</p>
+
+ <div class="card-container">
+        <div class="w-33">
+            <div class="card">
+                <div class="card-img">
+                    <img src="images/pexels-fauxels-3183197.jpg" alt="metting">
+                </div>
+                <div class="card-desc">
+                    <h2>Web Ground</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, ab reprehenderit, eveniet delectus consectetur fuga magnam soluta minus modi fugit est quibusdam dolorum perspiciatis, maxime facere! Quos quidem voluptas quo?</p>
+                    <a href="https://www.linkedin.com/in/md-abu-suffian-aa8132263/">Link Linkedin</a>
+                </div>
+            </div>
+        </div>
+        <div class="w-33">
+            <div class="card">
+                <div class="card-img">
+                    <img src="images/pexels-fauxels-3184360.jpg" alt="metting">
+                </div>
+                <div class="card-desc">
+                    <h2>Web Ground</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, ab reprehenderit, eveniet delectus consectetur fuga magnam soluta minus modi fugit est quibusdam dolorum perspiciatis, maxime facere! Quos quidem voluptas quo?</p>
+                    <a href="https://www.linkedin.com/in/md-abu-suffian-aa8132263/">Link Linkedin</a>
+                </div>
+            </div>
+        </div>
+        <div class="w-33">
+            <div class="card">
+                <div class="card-img">
+                    <img src="images/pexels-yan-krukau-7693107.jpg" alt="metting">
+                </div>
+                <div class="card-desc">
+                    <h2>Web Ground</h2>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, ab reprehenderit, eveniet delectus consectetur fuga magnam soluta minus modi fugit est quibusdam dolorum perspiciatis, maxime facere! Quos quidem voluptas quo?</p>
+                    <a href="https://www.linkedin.com/in/md-abu-suffian-aa8132263/">Link Linkedin</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    </section>
+ <!-- Card area end -->
+<!-- contact-area start -->
+
+<section class="contact-area">
+<div class="container">
+<h2>GET IN TOUCH</h2>
+<hr />
+<p>1600 Pennsylvania Ave NW, Washington, DC 
+20500, United States 
+of America. Tel: (202) 456-1111</p>
+ 
+<div class="contact-boxes">
+ 
+ <form action="">
+   
+   <div class="user-name">
+   <div class="name-field">
+   <label for="name" >Your Name <span>*</span> </label>
+   <input type="text" id="name" />
+   </div>
+   <div class="email-field">
+  <label for="email" >Your email <span>*</span> </label>
+   <input type="email" id="email" />
+   </div>
+   </div>
+  <textarea placeholder="Your Name" name="" id="" cols="30" rows="8"></textarea>
+  
+   <div class="Massage">
+   
+   <input type="submit" value="Send Massage" />
+   
+   </div>
+ 
+ </form>
+
+
+</div>
+</div>
+</section>
+
+<div class="footer-area">
+
+<div class="container">
+
+<nav class="footer-menu">
+
+<ul>
+	<li><a href="#">Facebook</a></li>
+	<li><a href="#">Twitter</a></li>
+	<li><a href="#">Google</a></li>
+	<li><a href="#">Linkedin</a></li>
+	<li><a href="#">Behance</a></li>
+	<li><a href="#">Dribble</a></li>
+	<li><a href="#">GitHub</a></li>
+</ul>
+
+</nav>
+
+</div>
+
+</div>
+ 
+ <script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+ <script type="text/javascript" src="js/uikit-icons.min.js"></script>
+ <script type="text/javascript" src="js/uikit.min.js"></script>
+ <script type="text/javascript" src="js/bootstrap.min.js"></script>
+ <script type="text/javascript" src="js/pie-chart.js"></script>
+ <script type="text/javascript" src="js/mixitup.min.js"></script>
+ <script type="text/javascript">
+ 
+  (function($){
+  
+  $(document).ready(function(){
+  
+  $('.user-name .name-field input').focus(function(){
+  
+  $('.name-field label').hide();
+  
+  });
+  
+  
+  $('.user-name .email-field input').focus(function(){
+  
+  $('.email-field label').hide();
+  
+  });
+  
+  $('.sk1').pieChart({
+  	barColor: '#30bae7',
+	trackColor:'#dfe8ed',
+	lineWidth:'12',
+	size:'160',
+	rotate:'0',
+	animate: {
+  duration: 2500,
+  enabled: true
+}
+  });
+  
+  $('.sk2').pieChart({
+  	barColor: '#d74680',
+	trackColor:'#dfe8ed',
+	lineWidth:'12',
+	size:'160',
+	rotate:'0',
+	animate: {
+  duration: 2000,
+  enabled: true
+}
+  });
+ 
+ $('.sk3').pieChart({
+  	barColor: '#15c7a8',
+	trackColor:'#dfe8ed',
+	lineWidth:'12',
+	size:'160',
+	rotate:'0',
+	animate: {
+  duration: 1500,
+  enabled: true
+}
+  });
+  
+  $('.sk4').pieChart({
+  	barColor: '#eb7d4b',
+	trackColor:'#dfe8ed',
+	lineWidth:'12',
+	size:'160',
+	rotate:'0',
+	animate: {
+  duration: 2000,
+  enabled: true
+}
+  });
+  
+  var mixingkorvo= mixitup('.protfloio-boxes')
+  
+  });
+ 
+  })(jQuery);
+ 
+ </script>
+  
+
+  <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+  <script src="https://www.google-analytics.com/analytics.js" async></script>
+</body>
+
+</html>
